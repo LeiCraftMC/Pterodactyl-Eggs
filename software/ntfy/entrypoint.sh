f3318632e85f0636ef20dc6411744702505a844b
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-03-28 23:12:33 +0000

    update

--- software/ntfy/entrypoint.sh
+++ software/ntfy/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+wget https://github.com/binwiederhier/ntfy/releases/download/v2.11.0/ntfy_2.11.0_linux_amd64.tar.gz
+tar zxvf ntfy_2.11.0_linux_amd64.tar.gz
+cp -a ntfy_2.11.0_linux_amd64/ntfy /usr/local/bin/ntfy
+mkdir /etc/ntfy && cp ntfy_2.11.0_linux_amd64/{client,server}/*.yml /etc/ntfy
+ntfy serve
