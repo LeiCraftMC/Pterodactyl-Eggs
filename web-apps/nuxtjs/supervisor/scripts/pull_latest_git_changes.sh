051a93f9a9e1d3483518a6c2618d33d4ef1c38fb
Linus Fischer <88381277+LeiCraft@users.noreply.github.com>
2025-11-27 19:37:55 +0000

    update

--- web-apps/nuxtjs/supervisor/scripts/pull_latest_git_changes.sh
+++ web-apps/nuxtjs/supervisor/scripts/pull_latest_git_changes.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 if [ -d "/home/container/.app/git-repo/.git" ]; then
+
     echo "Pulling latest changes from Git repository..."
+
     cd /home/container/.app/git-repo
+
+    git stash
     git pull
+
     cd /home/container
+    
 else 
     echo "No Git repository found to pull changes from."
     exit 1
