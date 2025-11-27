b4d5a52f98768f4d32f9b0921787bf38777546af
Linus Fischer <88381277+LeiCraft@users.noreply.github.com>
2025-11-27 17:12:58 +0000

    update

---
+++ web-apps/nuxtjs/supervisor/scripts/move_build_to_instance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTANCE_NUMBER=$1
+
+if [ ${INSTANCE_NUMBER} != "1" ] && [ ${INSTANCE_NUMBER} != "2" ]; then
+    echo "Invalid instance number provided. Must be 1 or 2."
+    exit 1
+fi
+
+if [ ! -d "/home/container/.app/git-repo/.output" ]; then
+    echo "No build found to move. Please create a build first."
+    exit 1
+fi
+
+rm -rf /home/container/.app/instance/${INSTANCE_NUMBER}/*
+
+cp -r /home/container/.app/git-repo/.output/* /home/container/.app/instance/${INSTANCE_NUMBER}/
--- web-apps/nuxtjs/supervisor/src/instance_handler.rs
+++ web-apps/nuxtjs/supervisor/src/instance_handler.rs
@@ -2,15 +2,20 @@ use crate::utils;
 use once_cell::sync::Lazy;
 use std::sync::RwLock;
 
-// #[derive(Debug)]
-// struct AppState {
-//     currentMainInstance:
+#[derive(Debug)]
+struct AppState {
+    currentMainInstance: String
+    instance1Proc: Option<utils::CommandHandle>,
+    instance2Proc: Option<utils::CommandHandle>,
+}
 
-// }
-
-// static STATE: Lazy<RwLock<AppState>> = Lazy::new(|| {
-
-// });
+static STATE: Lazy<RwLock<AppState>> = Lazy::new(|| {
+    RwLock::new(AppState {
+        currentMainInstance: "1".to_string(),
+        instance1Proc: None,
+        instance2Proc: None,
+    })
+});
 
 pub struct InstanceHandler {}
 
@@ -21,16 +26,25 @@ impl InstanceHandler {
         if let Err(e) = pull_latest_git_changes_proc.wait().await {
             eprintln!("Error pulling latest git changes: {}", e);
         }
-
-        let create_new_build_proc = utils::run_cmd_with_logs("/usr/local/share/supervisor/scripts/create_new_build.sh", &[], &[]);
-        if let Err(e) = create_new_build_proc.wait().await {
-            eprintln!("Error creating new build: {}", e);
-        }
-        
+  
         let cleanup_instances_proc = utils::run_cmd_with_logs("/usr/local/share/supervisor/scripts/cleanup_instances.sh", &[], &[]);
         if let Err(e) = cleanup_instances_proc.wait().await {
             eprintln!("Error cleaning up instances: {}", e);
         }
 
+        let create_new_build_proc = utils::run_cmd_with_logs("/usr/local/share/supervisor/scripts/create_new_build.sh", &[], &[]);
+        if let Err(e) = create_new_build_proc.wait().await {
+            eprintln!("Error creating new build: {}", e);
+        }
+
+        let move_build_to_instance_proc = utils::run_cmd_with_logs("/usr/local/share/supervisor/scripts/move_build_to_instance.sh", &["1"], &[]);
+        if let Err(e) = move_build_to_instance_proc.wait().await {
+            eprintln!("Error moving build to instance 1: {}", e);
+        }
+
+        {
+            let mut state = STATE.write().unwrap();
+            state.instance1Proc = Some(utils::run_cmd_with_logs("/usr/local/share/supervisor/scripts/start_instance.sh", &["1"], &[]));
+        }
     }
 }
