a28edd801136563669d1b18db340dbd9c7fec811
Linus Fischer <88381277+LeiCraft@users.noreply.github.com>
2025-12-15 22:04:08 +0000

    update

--- software/pocketbase/entrypoint.sh
+++ software/pocketbase/entrypoint.sh
@@ -3,9 +3,9 @@
 function check_cpu_arch {
     local arch=$(uname -m)
     if [ "$arch" = "x86_64" ]; then
-        declare -g ARCH="linux-amd64"
+        declare -g ARCH="linux_amd64"
     elif [ "$arch" = "aarch64" ]; then
-        declare -g ARCH="linux-arm64"
+        declare -g ARCH="linux_arm64"
     else
         echo "Unsupported architecture: $arch"
         exit 1
@@ -39,42 +39,46 @@ function get_latest_version {
 
 function get_current_version {
     # output is in format pocketbase version 0.29.2
-    local version=$(./pocketbase -v 2>/dev/null)
+    local version=$(./.bin/pocketbase -v 2>/dev/null)
     echo $version | awk '{print $3}'
 }
 
 function download_app {
     local version=$1
+    local version_without_v=${version#v}
     local arch=$2
-    local name="pocketbase-${arch}"
-    local url="https://github.com/pocketbase/pocketbase/releases/download/${version}/${name}.tar.gz"
+    local name="pocketbase_${version_without_v}_${arch}"
+    local url="https://github.com/pocketbase/pocketbase/releases/download/${version}/${name}.zip"
 
     echo "Downloading pocketbase version $version for architecture $arch..."
 
-    http_response_code="$(curl --write-out '%{http_code}' -sL -o $name.tar.gz "$url")"
+    http_response_code="$(curl --write-out '%{http_code}' -sL -o $name.zip "$url")"
 
     if [ "$http_response_code" != "200" ]; then
         echo "Failed to download pocketbase binary. HTTP response code: $http_response_code"
+        rm -f $name.zip
         exit 1
     fi
 
     echo "Download complete. Extracting..."
-    tar zxvf "${name}.tar.gz"
+
+    unzip -o $name.zip -d tmp-$name
     if [ $? -ne 0 ]; then
         echo "Failed to extract pocketbase binary."
         exit 1
     fi
     
+    mv tmp-$name/pocketbase /home/container/.bin/pocketbase
     chmod +x /home/container/.bin/pocketbase
 
-    rm "${name}.tar.gz"
-    rm -rf $name
+    rm "${name}.zip"
+    rm -rf tmp-$name
 
     echo "pocketbase $version downloaded successfully."
 }
 
 function create_directories {
-    # mkdir -p /home/container/conf
+    mkdir -p /home/container/.bin
 }
 
 function main {
@@ -105,7 +109,7 @@ function main {
         fi
     else
         
-        if [[ "v$VERSION" != "$LOCAL_VERSION" ]]; then
+        if [[ "$VERSION" != "$LOCAL_VERSION" ]]; then
             echo "Requested version $VERSION is not installed. Downloading..."
             download_app v$VERSION $ARCH
         else
